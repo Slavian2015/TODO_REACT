8f464ff8e91a3ff775a6e78679c58ff840879bc9
winsbar <slavaku2014@gmail.com>
2022-12-10 16:58:53 +0200

    feat(api): updated DB connection url3

--- server/migration/env.py
+++ server/migration/env.py
@@ -8,7 +8,7 @@ from alembic import context
 
 from src.mapping import PublicMetadata
 
-load_dotenv(dotenv_path=Path(__file__).absolute() / '.env')
+load_dotenv(dotenv_path=Path(__file__).parent.parent.absolute() / '.env')
 
 config = context.config
 
@@ -59,9 +59,9 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    print("\n\nENV URL  -->>", Path(__file__).absolute(), "\n\n")
-    print("\n\nPOSTGRESQL_DSN  -->>", os.getenv('POSTGRESQL_DSN'), "\n\n")
-    connectable = create_engine(os.getenv('POSTGRESQL_DSN'))
+    print("\n\nENV URL  -->>", Path(__file__).parent.parent.absolute(), "\n\n")
+    print("\n\nPOSTGRESQL_DSN  -->>", os.getenv('DATABASE_URL'), "\n\n")
+    connectable = create_engine(os.getenv('DATABASE_URL'))
 
     with connectable.connect() as connection:
         context.configure(
